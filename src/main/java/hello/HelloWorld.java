74fe0d9746f6fb1ff27100e359248f0fd355541a
Fanfan <821078797@qq.com>
2016-11-28 23:04:04 +0800

    create helloworld

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by YQF on 2016/11/28.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloWold for project test");
+    }
+}
---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by YQF on 2016/11/28.
+ */
+public class SimpleTest {
+    @Test
+    public void nothing_to_test() throws Exception {
+        //nothing to test yet
+
+    }
+}
